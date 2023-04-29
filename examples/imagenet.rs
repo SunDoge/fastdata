2372ed33816c8c606ce9ccccdba6036b865393cd
SunDoge <384813529@qq.com>
2023-04-30 01:54:00 +0800

    save image

--- examples/imagenet.rs
+++ examples/imagenet.rs
@@ -25,15 +25,15 @@ impl Aug {
 }
 
 fn main() {
-    let vips_app = libvips::VipsApp::new("aug", false).unwrap();
-    dbg!(
-        vips_app.concurency_get(),
-        vips_app.cache_get_max(),
-        vips_app.cache_get_max_files(),
-        vips_app.cache_get_max_mem(),
-    );
+    // let vips_app = libvips::VipsApp::new("aug", false).unwrap();
+    // dbg!(
+    //     vips_app.concurency_get(),
+    //     vips_app.cache_get_max(),
+    //     vips_app.cache_get_max_files(),
+    //     vips_app.cache_get_max_mem(),
+    // );
 
-    vips_app.concurrency_set(2);
+    // vips_app.concurrency_set(2);
     // vips_app.cache_set_max(10000000);
     // vips_app.cache_set_max_files(0);
     // vips_app.cache_set_max_mem(1024 * 1024 * 1024 * 1024);
@@ -60,22 +60,22 @@ fn main() {
     let start_time = Instant::now();
     let num_records = tfrecords
         .take(10)
-        .map(|path| {
+        .flat_map(|path| {
             let path = path.unwrap();
             println!("tfrecord: {}", path.display());
             let reader = TfRecordReader::open(&path).expect("fail to open");
             reader
         })
-        .flat_map(|r| r)
         .par_bridge()
         .map(|buf| {
             let example =
                 fastdata::tensorflow::Example::decode(&mut Cursor::new(buf.unwrap())).unwrap();
             let image_bytes = example.get_bytes_list("image")[0];
             let label = example.get_int64_list("label")[0];
+            
 
-            let img = VipsImage::new_from_buffer(image_bytes, "").unwrap();
-            let img = aug.apply(&img).unwrap();
+            // let img = VipsImage::new_from_buffer(image_bytes, "").unwrap();
+            // let img = aug.apply(&img).unwrap();
             // let image_buffer = img.image_write_to_memory();
             label
         })
