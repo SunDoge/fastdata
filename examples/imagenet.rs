638c6b130b416a887d63d1fbbbb01cebdf49320c
SunDoge <384813529@qq.com>
2023-04-30 22:58:28 +0800

    switch to rkvy

--- examples/imagenet.rs
+++ examples/imagenet.rs
@@ -8,6 +8,7 @@ use rayon::prelude::{ParallelBridge, ParallelIterator};
 
 fn main() {
     opencv::core::set_num_threads(0).unwrap();
+    opencv::core::set_use_opencl(false).unwrap();
     let build_info = opencv::core::get_build_information().unwrap();
     println!("{}", &build_info);
 
@@ -28,30 +29,29 @@ fn main() {
             let reader = TfRecordReader::open(&path).expect("fail to open");
             reader
         })
-        .shuffle(10000)
+        // .shuffle(10000)
         .par_bridge()
         .map(|buf| {
             let example =
                 fastdata::tensorflow::Example::decode(&mut Cursor::new(buf.unwrap())).unwrap();
             let image_bytes = example.get_bytes_list("image")[0];
             let label = example.get_int64_list("label")[0];
-            let mat_buf = Mat::from_slice(image_bytes).unwrap();
-            let img =
-                opencv::imgcodecs::imdecode(&mat_buf, opencv::imgcodecs::IMREAD_COLOR).unwrap();
-            let mut rgb_img = Mat::default();
-
-            opencv::imgproc::cvt_color(&img, &mut rgb_img, opencv::imgproc::COLOR_BGR2RGB, 0)
-                .unwrap();
-            let mut resized = Mat::default();
-            opencv::imgproc::resize(
-                &rgb_img,
-                &mut resized,
-                (256, 256).into(),
-                0.0,
-                0.0,
-                opencv::imgproc::INTER_LINEAR,
-            )
-            .unwrap();
+            // let mat_buf = Mat::from_slice(image_bytes).unwrap();
+            // let img =
+            //     opencv::imgcodecs::imdecode(&mat_buf, opencv::imgcodecs::IMREAD_COLOR).unwrap();
+            // let mut rgb_img = Mat::default();
+            // opencv::imgproc::cvt_color(&img, &mut rgb_img, opencv::imgproc::COLOR_BGR2RGB, 0)
+            //     .unwrap();
+            // let mut resized = Mat::default();
+            // opencv::imgproc::resize(
+            //     &rgb_img,
+            //     &mut resized,
+            //     (256, 256).into(),
+            //     0.0,
+            //     0.0,
+            //     opencv::imgproc::INTER_LINEAR,
+            // )
+            // .unwrap();
             label
         })
         .count();
