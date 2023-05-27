48f42c3815fedfb2cb6d52fcfc9b2cbfc02e0038
SunDoge <384813529@qq.com>
2023-05-27 12:02:19 +0800

    save

--- fastdata-tfrecord/examples/make_tfrecord.rs
+++ fastdata-tfrecord/examples/make_tfrecord.rs
@@ -1,6 +1,9 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, fs::File, io::Read, path::PathBuf};
 
 use clap::Parser;
+use fastdata_tfrecord::sync_writer::TfrecordWriter;
+use kanal::bounded;
+use rayon::{prelude::{ParallelBridge, ParallelIterator, IntoParallelRefIterator, IndexedParallelIterator}, slice::ParallelSlice};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -15,4 +18,64 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     dbg!(&cli);
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(cli.num_threads)
+        .build_global()
+        .unwrap();
+
+    let mut dirs: Vec<_> = cli
+        .in_dir
+        .read_dir()
+        .unwrap()
+        .filter_map(|path| {
+            let path = path.unwrap();
+            if let Ok(file_type) = path.file_type() {
+                if file_type.is_dir() {
+                    Some(path.file_name())
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    dirs.sort();
+
+    let class_map: HashMap<_, _> = dirs
+        .iter()
+        .enumerate()
+        .map(|(i, p)| (p.as_os_str(), i))
+        .collect();
+
+    // let (sender, receiver) = bounded(1024 * 1024);
+
+    let pattern = cli.in_dir.join("*/*");
+
+    let samples: Vec<_> = glob::glob(pattern.to_str().unwrap())
+        .unwrap()
+        .par_bridge()
+        .map(|path| {
+            let path = path.unwrap();
+            let class_name = path.parent().unwrap().file_name().unwrap();
+            let class_index = class_map[class_name];
+
+            // let mut img_buf = Vec::new();
+            // File::open(&path)
+            //     .unwrap()
+            //     .read_to_end(&mut img_buf)
+            //     .unwrap();
+
+            (path, class_index)
+        })
+        .collect();
+
+    dbg!(samples.len());
+
+    samples.par_chunks(500).enumerate().map(|(index, chunk)| {
+        // let mut writer = TfrecordWriter::create(path)
+    });
+
 }
--- fastdata-tfrecord/src/async_reader.rs
+++ fastdata-tfrecord/src/async_reader.rs
@@ -1,2 +1,3 @@
 pub mod io_uring_multi_files;
+pub mod io_uring_random_reader;
 pub mod io_uring_single_file;
